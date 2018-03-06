a78f2dead03e31ec8aac4a6f5a0f51ca9fc16f42
XPocin <npaavilainen91@gmail.com>
2018-03-06 12:52:48 +0200

    Add files via upload

--- skriptit.js
+++ skriptit.js
@@ -1,8 +1,23 @@
-var kamut = ["Mauno", "Pekka", "Jarmo","Kalle","Valtteri"];
+var kamut = [];
+
+function jarjestaFrendit(){
+    kamut.sort();
+    console.log(kamut);
+    listaaKaverit();
+}
+
+function poistaKaveri() {
+    document.getElementById("kamut");
+    kamut.pop();
+    console.log(kamut);
+    listaaKaverit();
+}
+
 
 function lisaaKaveri(){
+
     var nimi = window.prompt("Anna kaverin nimi");
-    kamut.push();
+    kamut.push(nimi);
     console.log(kamut);
 
     listaaKaverit();
@@ -10,13 +25,29 @@ function lisaaKaveri(){
 
 function listaaKaverit(){
 
-    document.getElementById('frendit').innerHTML = '';
+    document.getElementById('kamut').innerHTML = '';
 
     for (var i=0; i < kamut.length; i++){
-        document.getElementById('frendit').innerHTML += '<li>' + kamut[i] + '</li>';
+        document.getElementById('kamut').innerHTML += '<li>' + kamut[i] + '</li>';
     }
 }
 
+        function myFunction() {
+            var input, filter, ul, li, a, i;
+            input = document.getElementById("nimi_kentta");
+            filter = input.value.toUpperCase();
+            ul = document.getElementById("kamut");
+            li = ul.getElementsByTagName("li");
+            for (i = 0; i < li.length; i++) {
+                a = li[i].getElementsByTagName("a")[0];
+                if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                    li[i].style.display = "";
+                } else {
+                    li[i].style.display = "none";
+        
+                }
+            }
+        }
 
 // kamut.push('yeet');
 
