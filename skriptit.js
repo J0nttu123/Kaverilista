3f0f049f7b6ea4e6cb1e5cf0a5aa792453fa4896
J0nttu123 <48908835+J0nttu123@users.noreply.github.com>
2019-08-19 11:38:58 +0300

    Update skriptit.js

--- skriptit.js
+++ skriptit.js
@@ -32,22 +32,5 @@ function listaaKaverit(){
     }
 }
 
-        function myFunction() {
-            var input, filter, ul, li, a, i;
-            input = document.getElementById("nimi_kentta");
-            filter = input.value.toUpperCase();
-            ul = document.getElementById("kamut");
-            li = ul.getElementsByTagName("li");
-            for (i = 0; i < li.length; i++) {
-                a = li[i].getElementsByTagName("a")[0];
-                if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-                    li[i].style.display = "";
-                } else {
-                    li[i].style.display = "none";
         
-                }
-            }
-        }
-
-// kamut.push('yeet');
 
